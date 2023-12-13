9a34a782546f23b9cd36c311b7febfe679d5ce2d
jyko <yeon0125@gmail.com>
2023-12-13 13:09:36 +0900

    feat. page 추가

---
+++ src/pages/PickList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DefaultLayout from '../layouts/default';
+
+function PickList() {
+  return (
+    // <DefaultLayout>
+    <h1>????????list?????????</h1>
+    // </DefaultLayout>
+  );
+}
+
+export default PickList;
