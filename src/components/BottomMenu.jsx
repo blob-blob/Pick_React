21875a61ab14bce3ce05ade70c4c1d738d0eac85
jyko <yeon0125@gmail.com>
2024-01-08 23:32:24 +0900

    feat. bottommenu link

--- src/components/BottomMenu.jsx
+++ src/components/BottomMenu.jsx
@@ -1,9 +1,12 @@
 import styles from '../styles/BottomMenu.module.scss';
+import { Link } from 'react-router-dom';
 
 const BottomMenu = () => {
   return (
     <div className={styles.bottom_menu}>
-      <div>aaaa</div>
+      <div>
+        <Link to="/">Home</Link>
+      </div>
       <div>bbbb</div>
       <div>cccc</div>
       <div>dddd</div>
