ef3dc366d01a90180512ba24be872cd612eab684
jeong hyun seok <jhs92043@gmail.com>
2024-01-26 23:57:20 +0900

    feat. 화면 스타일 변경중

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,28 @@
+import React, { useState } from 'react';
 import DefaultLayout from '../layouts/Default';
 
 function Home() {
+  const [selectedTab, setSelectedTab] = useState('collections'); // collections 또는 follows
+
   return (
     <DefaultLayout>
-      <h1>Home</h1>
+      <div className="w-full">
+        <div className="flex flex-col items-start pt-4 pl-4">
+          <button
+            className="bg-white focus:outline-none"
+            style={{ fontWeight: selectedTab === 'collections' ? 'bold' : 'normal' }}
+            onClick={() => setSelectedTab('collections')}>
+            내컬렉션
+          </button>
+          <button
+            className="bg-white focus:outline-none"
+            style={{ fontWeight: selectedTab === 'follows' ? 'bold' : 'normal' }}
+            onClick={() => setSelectedTab('follows')}>
+            팔로우
+          </button>
+        </div>
+        <div>{selectedTab === 'collections' ? 'collections' : 'follows'}</div>
+      </div>
     </DefaultLayout>
   );
 }
