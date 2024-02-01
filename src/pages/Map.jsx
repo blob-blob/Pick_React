d22211b4afb96fd7cc4c842723173303e3f6104a
jeong hyun seok <jhs92043@gmail.com>
2024-02-01 23:53:57 +0900

    feat. react map 실패

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -1,14 +1,44 @@
-import React from 'react';
+import { useEffect, useRef } from 'react';
 import DefaultLayout from '../layouts/Default';
 import { useNavigate } from 'react-router-dom';
 
+const { naver } = window;
+
+window.addEventListener(
+  'mousewheel',
+  function () {
+    // 이벤트 핸들러의 코드
+    console.log('mousewheel');
+  },
+  { passive: true },
+);
 function AddCollection() {
   const navigate = useNavigate();
+  const mapElement = useRef(null);
+
+  useEffect(() => {
+    if (!mapElement.current || !naver) return;
+
+    // 지도에 표시할 위치의 위도와 경도 좌표를 파라미터로 넣어줍니다.
+    const location = new naver.maps.LatLng(37.5656, 126.9769);
+    const mapOptions = {
+      center: location,
+      zoom: 17,
+      zoomControl: true,
+    };
+
+    const map = new naver.maps.Map(mapElement.current, mapOptions);
+    new naver.maps.Marker({
+      position: location,
+      map,
+    });
+  }, []);
+
   return (
     <DefaultLayout>
       <div className="w-full">
         <div className="flex justify-around my-4">
-          <div>지도</div>
+          <div ref={mapElement} style={{ minHeight: '400px' }} />
         </div>
       </div>
       {/* 지도보기 버튼 */}
