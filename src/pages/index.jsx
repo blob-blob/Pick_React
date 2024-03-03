dc6f0a84a65dafadc71668d792040196d82604b4
jeong hyun seok <jhs92043@gmail.com>
2024-03-03 15:17:54 +0900

    feat. 경로 jsx 추가

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,17 +1,17 @@
 // 홈, 마이페이지
-export { default as Home } from './Home';
-export { default as MyInfo } from './MyInfo';
+export { default as Home } from './Home.jsx';
+export { default as MyInfo } from './MyInfo.jsx';
 
 // 로그인 페이지
-export { default as Login } from './Login';
-export { default as OAuth } from './OAuth';
+export { default as Login } from './Login.jsx';
+export { default as OAuth } from './OAuth.jsx';
 
 // 404 not found
-export { default as NotFound } from './NotFound';
+export { default as NotFound } from './NotFound.jsx';
 
 // 컬렉션 추가
-export { default as AddCollection } from './AddCollection';
+export { default as AddCollection } from './AddCollection.jsx';
 
 // 지도
 
-export { default as Map } from './Map';
+export { default as Map } from './Map.jsx';
