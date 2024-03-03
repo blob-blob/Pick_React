1c182dc47cfc5d7a4463803ac5d467fde6416f0b
jeong hyun seok <jhs92043@gmail.com>
2024-03-03 15:37:15 +0900

    feat. 절대경로 수정1

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,17 +1,16 @@
 // 홈, 마이페이지
-export { default as Home } from '../pages/Home.jsx';
-export { default as MyInfo } from '../pages/MyInfo.jsx';
+export { default as Home } from '/src/pages/Home.jsx';
+export { default as MyInfo } from '/src/pages/MyInfo.jsx';
 
 // 로그인 페이지
-export { default as Login } from '../pages/Login.jsx';
-export { default as OAuth } from '../pages/OAuth.jsx';
+export { default as Login } from '/src/pages/Login.jsx';
+export { default as OAuth } from '/src/pages/OAuth.jsx';
 
 // 404 not found
-export { default as NotFound } from '../pages/NotFound.jsx';
+export { default as NotFound } from '/src/pages/NotFound.jsx';
 
 // 컬렉션 추가
-export { default as AddCollection } from '../pages/AddCollection.jsx';
+export { default as AddCollection } from '/src/pages/AddCollection.jsx';
 
 // 지도
-
-export { default as Map } from '../pages/Map.jsx';
+export { default as Map } from '/src/pages/Map.jsx';
